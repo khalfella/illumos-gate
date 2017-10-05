39910d73789d0244ff1886f3e4e8d003566fffae
Mohamed A. Khalfella <khalfella@gmail.com>
2017-10-04 18:17:11 -0600

    Added the necessary framework to show the bay information. I had no way to test this work in this VM since it doesn't show bay information.
    TODO:
    	- Test the code in a machine that has many slots
    	- Make sure to free the space once the command is over
    	- Review the code for any kind of errors.
    	- Carefully review the memory allocation and NULL strings

--- usr/src/cmd/diskinfo/diskinfo.c
+++ usr/src/cmd/diskinfo/diskinfo.c
@@ -30,6 +30,7 @@
 #include <sys/nvpair.h>
 #include <sys/param.h>
 #include <sys/ccompile.h>
+#include <sys/avl.h>
 
 #include <fm/libtopo.h>
 #include <fm/topo_hc.h>
@@ -43,14 +44,35 @@ static boolean_t g_Pflag = B_FALSE;	/* Physical  */
 static boolean_t g_pflag = B_FALSE;	/* Parseable */
 
 typedef struct di_phys {
-	const char *dp_dev;
-	const char *dp_serial;
-	const char *dp_slotname;
+	uint64_t dp_size;
+	uint32_t dp_blksize;
+	char *dp_vid;
+	char *dp_pid;
+	boolean_t dp_removable;
+	boolean_t dp_ssd;
+	char *dp_dev;
+	char *dp_ctype;
+	char *dp_serial;
+	char *dp_slotname;
 	int dp_chassis;
 	int dp_slot;
 	int dp_faulty;
 	int dp_locate;
+	avl_node_t dp_tnode;
 } di_phys_t;
+
+static avl_tree_t g_disks;
+
+static int
+avl_di_comp(const void *di1, const void *di2)
+{
+	const di_phys_t *di1p = di1;
+	const di_phys_t *di2p = di2;
+	int cmp = strcmp(di1p->dp_dev, di2p->dp_dev);
+	if (cmp == 0)
+		return (0);
+	return (cmp/abs(cmp));
+}
 
 static void __NORETURN
 fatal(int rv, const char *fmt, ...)
@@ -104,10 +126,54 @@ condensed_tristate(int val, char c)
 
 	return ('?');
 }
+
+static int
+bay_walker(topo_hdl_t *hp, tnode_t *np, void *arg)
+{
+	int err;
+	di_phys_t *dip;
+	tnode_t *pnp;
+	char *slotname;
+	boolean_t found = B_FALSE;
+	int slot, chassis;
+
+	if (strcmp(topo_node_name(np), BAY) != 0)
+		return (TOPO_WALK_NEXT);
+
+	pnp = topo_node_parent(np);
+	slot = topo_node_instance(np);
+	chassis = topo_node_instance(pnp);
+
+	for (dip = avl_first(&g_disks); dip != NULL;
+	    dip = AVL_NEXT(&g_disks, dip)) {
+		if (dip->dp_slot == slot && dip->dp_chassis == chassis) {
+			found = B_TRUE;
+			break;
+		}
+	}
+
+	if (!found) {
+		dip = malloc(sizeof(di_phys_t));
+		assert(dip != NULL);
+		memset(dip, 0, sizeof(di_phys_t));
+		dip->dp_slot = slot;
+		dip->dp_chassis = chassis;
+
+		if (topo_prop_get_string(pnp, TOPO_PGROUP_PROTOCOL,
+		    TOPO_PROP_LABEL, &slotname, &err) == 0) {
+			dip->dp_slotname = strdup(slotname);
+		}
+		avl_add(&g_disks, dip);
+	}
+
+	return (TOPO_WALK_NEXT);
+}
+
 static int
 disk_walker(topo_hdl_t *hp, tnode_t *np, void *arg)
 {
-	di_phys_t *pp = arg;
+	di_phys_t di;
+	di_phys_t *dip;
 	tnode_t *pnp;
 	tnode_t *ppnp;
 	topo_faclist_t fl;
@@ -115,22 +181,21 @@ disk_walker(topo_hdl_t *hp, tnode_t *np, void *arg)
 	int err;
 	topo_led_state_t mode;
 	topo_led_type_t type;
-	char *name, *slotname, *serial;
 
 	if (strcmp(topo_node_name(np), DISK) != 0)
 		return (TOPO_WALK_NEXT);
 
 	if (topo_prop_get_string(np, TOPO_PGROUP_STORAGE,
-	    TOPO_STORAGE_LOGICAL_DISK_NAME, &name, &err) != 0) {
+	    TOPO_STORAGE_LOGICAL_DISK_NAME, &dip->dp_dev, &err) != 0) {
 		return (TOPO_WALK_NEXT);
 	}
 
-	if (strcmp(name, pp->dp_dev) != 0)
+	if ((dip = avl_find(&g_disks, &di, NULL)) == NULL)
 		return (TOPO_WALK_NEXT);
 
 	if (topo_prop_get_string(np, TOPO_PGROUP_STORAGE,
-	    TOPO_STORAGE_SERIAL_NUM, &serial, &err) == 0) {
-		pp->dp_serial = serial;
+	    TOPO_STORAGE_SERIAL_NUM, &dip->dp_serial, &err) == 0) {
+		dip->dp_serial = strdup(dip->dp_serial);
 	}
 
 	pnp = topo_node_parent(np);
@@ -158,10 +223,10 @@ disk_walker(topo_hdl_t *hp, tnode_t *np, void *arg)
 
 				switch (type) {
 				case TOPO_LED_TYPE_SERVICE:
-					pp->dp_faulty = mode ? 1 : 0;
+					dip->dp_faulty = mode ? 1 : 0;
 					break;
 				case TOPO_LED_TYPE_LOCATE:
-					pp->dp_locate = mode ? 1 : 0;
+					dip->dp_locate = mode ? 1 : 0;
 					break;
 				default:
 					break;
@@ -170,72 +235,124 @@ disk_walker(topo_hdl_t *hp, tnode_t *np, void *arg)
 		}
 
 		if (topo_prop_get_string(pnp, TOPO_PGROUP_PROTOCOL,
-		    TOPO_PROP_LABEL, &slotname, &err) == 0) {
-			pp->dp_slotname = slotname;
+		    TOPO_PROP_LABEL, &dip->dp_slotname, &err) == 0) {
+			dip->dp_slotname = strdup(dip->dp_slotname);
 		}
 
-		pp->dp_slot = topo_node_instance(pnp);
+		dip->dp_slot = topo_node_instance(pnp);
 	}
 
-	pp->dp_chassis = topo_node_instance(ppnp);
+	dip->dp_chassis = topo_node_instance(ppnp);
 
-	return (TOPO_WALK_TERMINATE);
-}
-
-static void
-populate_physical(topo_hdl_t *hp, di_phys_t *pp)
-{
-	int err;
-	topo_walk_t *wp;
-
-	pp->dp_faulty = pp->dp_locate = -1;
-	pp->dp_chassis = pp->dp_slot = -1;
-
-	err = 0;
-	wp = topo_walk_init(hp, FM_FMRI_SCHEME_HC, disk_walker, pp, &err);
-	if (wp == NULL) {
-		fatal(-1, "unable to initialise topo walker: %s",
-		    topo_strerror(err));
-	}
-
-	while ((err = topo_walk_step(wp, TOPO_WALK_CHILD)) == TOPO_WALK_NEXT)
-		;
-
-	if (err == TOPO_WALK_ERR)
-		fatal(-1, "topo walk failed");
-
-	topo_walk_fini(wp);
+	return (TOPO_WALK_NEXT);
 }
 
 static void
 enumerate_disks()
 {
 	topo_hdl_t *hp;
+	topo_walk_t *wp;
 	dm_descriptor_t *media;
 	int err, i;
 	int filter[] = { DM_DT_FIXED, -1 };
 	dm_descriptor_t *disk, *controller;
 	nvlist_t *mattrs, *dattrs, *cattrs = NULL;
 
-	uint64_t size, total;
-	uint32_t blocksize;
-	double total_in_GiB;
-	char sizestr[32];
-	char slotname[32];
-	char statestr[8];
-
-	char *vid, *pid, *opath, *c, *ctype = NULL;
-	boolean_t removable;
-	boolean_t ssd;
-	char device[MAXPATHLEN];
-	di_phys_t phys;
+	char *s, *c;
+	di_phys_t* dip;
 	size_t len;
+
+	avl_create(&g_disks, avl_di_comp, sizeof(di_phys_t),
+	    offsetof(di_phys_t, dp_tnode));
 
 	err = 0;
 	if ((media = dm_get_descriptors(DM_MEDIA, filter, &err)) == NULL) {
 		fatal(-1, "failed to obtain media descriptors: %s\n",
 		    strerror(err));
 	}
+
+	for (i = 0; media != NULL && media[i] != NULL; i++) {
+		if ((disk = dm_get_associated_descriptors(media[i],
+		    DM_DRIVE, &err)) == NULL) {
+			continue;
+		}
+
+		dip = malloc(sizeof(di_phys_t));
+		assert(dip != NULL);
+		memset(dip, 0, sizeof(di_phys_t));
+
+		mattrs = dm_get_attributes(media[i], &err);
+		err = nvlist_lookup_uint64(mattrs, DM_SIZE, &dip->dp_size);
+		assert(err == 0);
+		err = nvlist_lookup_uint32(mattrs, DM_BLOCKSIZE,
+		    &dip->dp_blksize);
+		assert(err == 0);
+		nvlist_free(mattrs);
+
+		dattrs = dm_get_attributes(disk[0], &err);
+
+		nvlist_query_string(dattrs, DM_VENDOR_ID, &dip->dp_vid);
+		nvlist_query_string(dattrs, DM_PRODUCT_ID, &dip->dp_pid);
+		nvlist_query_string(dattrs, DM_OPATH, &dip->dp_dev);
+
+		dip->dp_vid = strdup(dip->dp_vid);
+		dip->dp_pid = strdup(dip->dp_pid);
+		dip->dp_dev = strdup(dip->dp_dev);
+
+		dip->dp_removable = B_FALSE;
+		if (nvlist_lookup_boolean(dattrs, DM_REMOVABLE) == 0)
+			dip->dp_removable = B_TRUE;
+
+		dip->dp_ssd = B_FALSE;
+		if (nvlist_lookup_boolean(dattrs, DM_SOLIDSTATE) == 0)
+			dip->dp_ssd = B_TRUE;
+
+		nvlist_free(dattrs);
+
+		if ((controller = dm_get_associated_descriptors(disk[0],
+		    DM_CONTROLLER, &err)) != NULL) {
+			cattrs = dm_get_attributes(controller[0], &err);
+			nvlist_query_string(cattrs, DM_CTYPE, &dip->dp_ctype);
+			dip->dp_ctype = strdup(dip->dp_ctype);
+			for (c = dip->dp_ctype; *c != '\0'; c++)
+				*c = toupper(*c);
+			nvlist_free(cattrs);
+		}
+
+		if (dip->dp_ctype == NULL)
+			dip->dp_ctype = strdup("");
+
+		dm_free_descriptors(controller);
+		dm_free_descriptors(disk);
+
+		/*
+		 * Parse full device path to only show the device name,
+		 * i.e. c0t1d0.  Many paths will reference a particular
+		 * slice (c0t1d0s0), so remove the slice if present.
+		 */
+		if ((c = strrchr(dip->dp_dev, '/')) != NULL) {
+			s = dip->dp_dev;
+			while ((*s++ = *++c))
+				;
+		}
+		len = strlen(dip->dp_dev);
+		if (dip->dp_dev[len - 2] == 's' &&
+		    dip->dp_dev[len - 1] >= '0' &&
+		    dip->dp_dev[len - 1] <= '9')
+			dip->dp_dev[len - 2] = '\0';
+
+		dip->dp_faulty = dip->dp_locate = -1;
+		dip->dp_chassis = dip->dp_slot = -1;
+		avl_add(&g_disks, dip);
+	}
+
+	dm_free_descriptors(media);
+
+
+	/*
+	 * Walk toplogy information to populate serial, chassis,
+	 * slot, faulty and locator information.
+	 */
 
 	err = 0;
 	hp = topo_open(TOPO_VERSION, NULL, &err);
@@ -251,149 +368,130 @@ enumerate_disks()
 		    topo_strerror(err));
 	}
 
-	for (i = 0; media != NULL && media[i] != NULL; i++) {
-		if ((disk = dm_get_associated_descriptors(media[i],
-		    DM_DRIVE, &err)) == NULL) {
-			continue;
-		}
 
-		mattrs = dm_get_attributes(media[i], &err);
-		err = nvlist_lookup_uint64(mattrs, DM_SIZE, &size);
-		assert(err == 0);
-		err = nvlist_lookup_uint32(mattrs, DM_BLOCKSIZE, &blocksize);
-		assert(err == 0);
-		nvlist_free(mattrs);
+	wp = topo_walk_init(hp, FM_FMRI_SCHEME_HC, disk_walker, NULL, &err);
+	if (wp == NULL) {
+		fatal(-1, "unable to initialise topo walker: %s",
+		    topo_strerror(err));
+	}
 
-		dattrs = dm_get_attributes(disk[0], &err);
+	while ((err = topo_walk_step(wp, TOPO_WALK_CHILD)) == TOPO_WALK_NEXT)
+		;
+	if (err == TOPO_WALK_ERR)
+		fatal(-1, "topo walk failed");
 
-		nvlist_query_string(dattrs, DM_VENDOR_ID, &vid);
-		nvlist_query_string(dattrs, DM_PRODUCT_ID, &pid);
-		nvlist_query_string(dattrs, DM_OPATH, &opath);
+	topo_walk_fini(wp);
 
-		removable = B_FALSE;
-		if (nvlist_lookup_boolean(dattrs, DM_REMOVABLE) == 0)
-			removable = B_TRUE;
 
-		ssd = B_FALSE;
-		if (nvlist_lookup_boolean(dattrs, DM_SOLIDSTATE) == 0)
-			ssd = B_TRUE;
+	/* Here the aflag should work */
+	wp = topo_walk_init(hp, FM_FMRI_SCHEME_HC, bay_walker, NULL, &err);
+	if (wp == NULL) {
+		fatal(-1, "unable to initialise topo walker: %s",
+		    topo_strerror(err));
+	}
 
-		if ((controller = dm_get_associated_descriptors(disk[0],
-		    DM_CONTROLLER, &err)) != NULL) {
-			cattrs = dm_get_attributes(controller[0], &err);
-			nvlist_query_string(cattrs, DM_CTYPE, &ctype);
-			ctype = strdup(ctype);
-			for (c = ctype; *c != '\0'; c++)
-				*c = toupper(*c);
-		}
+	while ((err = topo_walk_step(wp, TOPO_WALK_CHILD)) == TOPO_WALK_NEXT)
+		;
+	if (err == TOPO_WALK_ERR)
+		fatal(-1, "topo walk failed");
 
-		/*
-		 * Parse full device path to only show the device name,
-		 * i.e. c0t1d0.  Many paths will reference a particular
-		 * slice (c0t1d0s0), so remove the slice if present.
-		 */
-		if ((c = strrchr(opath, '/')) != NULL)
-			(void) strlcpy(device, c + 1, sizeof (device));
-		else
-			(void) strlcpy(device, opath, sizeof (device));
-		len = strlen(device);
-		if (device[len - 2] == 's' &&
-		    (device[len - 1] >= '0' && device[len - 1] <= '9'))
-			device[len - 2] = '\0';
+	topo_walk_fini(wp);
 
-		bzero(&phys, sizeof (phys));
-		phys.dp_dev = device;
-		populate_physical(hp, &phys);
+	topo_snap_release(hp);
+	topo_close(hp);
+}
 
+static void
+show_disks()
+{
+	uint64_t total;
+	double total_in_GiB;
+	char *sizestr = NULL, *slotname = NULL, *statestr = NULL;
+	di_phys_t *dip;
+
+	for (dip = avl_first(&g_disks); dip != NULL;
+	    dip = AVL_NEXT(&g_disks, dip)) {
 		/*
 		 * The size is given in blocks, so multiply the number
 		 * of blocks by the block size to get the total size,
 		 * then convert to GiB.
 		 */
-		total = size * blocksize;
+		total = dip->dp_size * dip->dp_blksize;
 
 		if (g_pflag) {
-			(void) snprintf(sizestr, sizeof (sizestr),
-			    "%llu", total);
+			(void) asprintf(&sizestr, "%llu", total);
 		} else {
-			total_in_GiB = (double)total /
-			    1024.0 / 1024.0 / 1024.0;
-			(void) snprintf(sizestr, sizeof (sizestr),
-			    "%7.2f GiB", total_in_GiB);
+			total_in_GiB = (double)total / 1024.0 / 1024.0 / 1024.0;
+			(void) asprintf(&sizestr, "%7.2f GiB", total_in_GiB);
 		}
 
 		if (g_pflag) {
-			(void) snprintf(slotname, sizeof (slotname), "%d,%d",
-			    phys.dp_chassis, phys.dp_slot);
-		} else if (phys.dp_slotname != NULL) {
-			(void) snprintf(slotname, sizeof (slotname),
-			    "[%d] %s", phys.dp_chassis, phys.dp_slotname);
+			(void) asprintf(&slotname, "%d,%d",
+			    dip->dp_chassis, dip->dp_slot);
+		} else if (dip->dp_slotname != NULL) {
+			(void) asprintf(&slotname, "[%d] %s",
+			    dip->dp_chassis, dip->dp_slotname);
 		} else {
-			slotname[0] = '-';
-			slotname[1] = '\0';
+			slotname = strdup("-");
 		}
 
 		if (g_cflag) {
-			(void) snprintf(statestr, sizeof (statestr), "%c%c%c%c",
-			    condensed_tristate(phys.dp_faulty, 'F'),
-			    condensed_tristate(phys.dp_locate, 'L'),
-			    condensed_tristate(removable, 'R'),
-			    condensed_tristate(ssd, 'S'));
+			(void) asprintf(&statestr, "%c%c%c%c",
+			    condensed_tristate(dip->dp_faulty, 'F'),
+			    condensed_tristate(dip->dp_locate, 'L'),
+			    condensed_tristate(dip->dp_removable, 'R'),
+			    condensed_tristate(dip->dp_ssd, 'S'));
 		}
 
 		if (g_Pflag) {
 			if (g_Hflag) {
 				printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
-				    device, vid, pid,
-				    display_string(phys.dp_serial),
-				    display_tristate(phys.dp_faulty),
-				    display_tristate(phys.dp_locate), slotname);
+				    dip->dp_dev, dip->dp_vid, dip->dp_pid,
+				    display_string(dip->dp_serial),
+				    display_tristate(dip->dp_faulty),
+				    display_tristate(dip->dp_locate), slotname);
 			} else {
 				printf("%-22s  %-8s %-16s "
 				    "%-20s %-3s %-3s %s\n",
-				    device, vid, pid,
-				    display_string(phys.dp_serial),
-				    display_tristate(phys.dp_faulty),
-				    display_tristate(phys.dp_locate), slotname);
+				    dip->dp_dev, dip->dp_vid, dip->dp_pid,
+				    display_string(dip->dp_serial),
+				    display_tristate(dip->dp_faulty),
+				    display_tristate(dip->dp_locate), slotname);
 			}
 		} else if (g_cflag) {
 			if (g_Hflag) {
 				printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
-				    ctype, device, vid, pid,
-				    display_string(phys.dp_serial),
+				    dip->dp_ctype, dip->dp_dev,
+				    dip->dp_vid, dip->dp_pid,
+				    display_string(dip->dp_serial),
 				    sizestr, statestr, slotname);
 			} else {
 				printf("%-7s %-22s  %-8s %-16s "
 				    "%-20s\n\t%-13s %-4s %s\n",
-				    ctype, device, vid, pid,
-				    display_string(phys.dp_serial),
+				    dip->dp_ctype, dip->dp_dev,
+				    dip->dp_vid, dip->dp_pid,
+				    display_string(dip->dp_serial),
 				    sizestr, statestr, slotname);
 			}
 		} else {
 			if (g_Hflag) {
 				printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
-				    ctype, device, vid, pid, sizestr,
-				    display_tristate(removable),
-				    display_tristate(ssd));
+				    dip->dp_ctype, dip->dp_dev,
+				    dip->dp_vid, dip->dp_pid, sizestr,
+				    display_tristate(dip->dp_removable),
+				    display_tristate(dip->dp_ssd));
 			} else {
 				printf("%-7s %-22s  %-8s %-16s "
-				    "%-13s %-3s %-3s\n", ctype, device,
-				    vid, pid, sizestr,
-				    display_tristate(removable),
-				    display_tristate(ssd));
+				    "%-13s %-3s %-3s\n",
+				    dip->dp_ctype, dip->dp_dev,
+				    dip->dp_vid, dip->dp_pid, sizestr,
+				    display_tristate(dip->dp_removable),
+				    display_tristate(dip->dp_ssd));
 			}
 		}
-
-		free(ctype);
-		nvlist_free(cattrs);
-		nvlist_free(dattrs);
-		dm_free_descriptors(controller);
-		dm_free_descriptors(disk);
+	free(sizestr); free(slotname); free(statestr);
+	sizestr = slotname = statestr = NULL;
 	}
-
-	dm_free_descriptors(media);
-	topo_snap_release(hp);
-	topo_close(hp);
 }
 
 int
@@ -444,6 +542,7 @@ main(int argc, char *argv[])
 	}
 
 	enumerate_disks();
+	show_disks();
 
 	return (0);
 }
