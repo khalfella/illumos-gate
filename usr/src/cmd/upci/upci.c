98c9984921c785375185a1ec4468a6cfd7f239ed
Mohamed A. Khalfella <khalfella@gmail.com>
2018-02-20 21:02:19 +0300

    upci command looks better

--- usr/src/cmd/upci/upci.c
+++ usr/src/cmd/upci/upci.c
@@ -18,6 +18,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
@@ -29,52 +30,86 @@
 #include <sys/upci.h>
 
 #define UPCI_DEV	"/devices/pci@0,0/pci15ad,1976@0:upci"
+#define UPCI_MAX_DEVS	10
 
-int
-main(void)
+
+/* Flags */
+static boolean_t g_lflag = B_FALSE;
+
+
+
+static int g_fd = -1;
+
+static int
+usage(int status)
 {
-	int i, fd;
+	fprintf(stderr, "Usage\nupci: {-l, -h}\n"
+	    "\t-h\t Show this help message\n"
+	    "\t-l\t List PCI devices controlled by upci\n");
+	exit(status);
+}
+
+static int
+upci_list_devices()
+{
+	int i, devcnt;
 	upci_cmd_t cmd;
-	char buff[100];
+	upci_devinfo_t *di;
 
-	if((fd = open(UPCI_DEV, O_RDONLY)) == -1) {
-		fprintf(stderr, "Failed to open upci device\n");
+	if ((di = malloc(sizeof (upci_devinfo_t) * UPCI_MAX_DEVS)) == NULL) {
+		fprintf(stderr, "Failed to allocate memory\n");
 		return (1);
 	}
 
-	memset(&cmd, 0, sizeof(upci_cmd_t));
+	cmd.cm_uobufsz = sizeof (upci_devinfo_t) * UPCI_MAX_DEVS;
+	cmd.cm_uobuff = (uint64_t)((intptr_t) di);
 
-	cmd.cm_uobuff =  (uint64_t) (buff - ((char*)NULL)); 
-
-	printf("sizeof(upci_cmd_t) = %d\n", sizeof(upci_cmd_t));
-	printf("buff = %p uobuff = %xll\n", buff, cmd.cm_uobuff);
-
-
-	if (ioctl(fd, UPCI_IOCTL_CFG_READ, &cmd) != 0) {
-		fprintf(stderr, "upci driver CFG_READ ioctl failed\n");
-		return (1);
-	}
-
-	printf("CFG_READ ioctl output = \"%s\"\n", cmd.cm_uobuff);
-
-	upci_devinfo_t devs[3];
-	cmd.cm_uobufsz = sizeof (devs);
-	cmd.cm_uobuff = (uint64_t) ((intptr_t) devs);
-
-	printf("cmd.cm_uobufsz = %xll cmd.cm_uobuff = %xll\n",
-	    cmd.cm_uobufsz, cmd.cm_uobuff);
-
-	if (ioctl(fd, UPCI_IOCTL_LIST_DEVICES, &cmd) != 0) {
+	if (ioctl(g_fd, UPCI_IOCTL_LIST_DEVICES, &cmd) != 0) {
 		fprintf(stderr, "upci driver LIST_DEVICES ioctl failed\n");
 		return (1);
 	}
 
-	for (i = 0; i < cmd.cm_uobufsz / sizeof (upci_devinfo_t); i++) {
-		printf("dev[%d]: di_devpath = \"%s\" flags = %x\n",
-		    i, devs[i].di_devpath, devs[i].di_flags);
+	devcnt = cmd.cm_uobufsz / sizeof (upci_devinfo_t);
+	printf("%ld devices to list:\n", devcnt);
+
+	for (i = 0; i < devcnt; i++) {
+		printf("dev[%d]: di_devpath = \"%-40s\" flags = %x\n",
+		    i, di[i].di_devpath, di[i].di_flags); }
+
+	free(di);
+	return (0);
+}
+
+int
+main(int argc, char **argv)
+{
+	int c;
+
+	while((c = getopt(argc, argv, "lh")) != EOF) {
+		switch (c) {
+		case 'l':
+			g_lflag = B_TRUE;
+		break;
+		case 'h':
+			return usage(0);
+		break;
+		default:
+			return usage(1);
+		break;
+		}
 	}
 
+	if((g_fd = open(UPCI_DEV, O_RDONLY)) == -1) {
+		fprintf(stderr, "Failed to open upci device \"%s\"\n",
+		    UPCI_DEV);
+		return (1);
+	}
 
+	/* Make sure the user has choosen acceptable flags combination */
 
-        return (0);
+	if (g_lflag) {
+		return upci_list_devices();
+	}
+
+	return usage(1);
 }
